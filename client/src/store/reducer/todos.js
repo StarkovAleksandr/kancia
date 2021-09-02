d1a5d9b51bd0fd5a372d7183e20c6f153d3f6d77
StarkovAleksandr <starkovlife@gmail.com>
2021-09-02 19:14:07 +0300

    change reducer

--- client/src/store/reducer/todos.js
+++ client/src/store/reducer/todos.js
@@ -1,14 +1,16 @@
 import { ADD_TODO, GET_TODOS } from '../actionTypes';
 
-const defaultState = [];
+const defaultState = {
+  todos: [],
+};
 
 export const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, [action.payload]];
+    // return [...state, [action.payload]];
 
     case GET_TODOS:
-      return [...state];
+    // return [...state];
 
     default:
       return state;
