ea9ddf777c3437ecee0d9b9b0554c0981bb638e0
StarkovAleksandr <starkovlife@gmail.com>
2021-09-03 11:40:45 +0300

    Refaktoring other

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,60 +2,27 @@ import React from 'react';
 
 import { TodoList } from './Todo/TodoList';
 import { Context } from './Сontext';
-import { Loader } from './Loader';
-import { Theme } from './ChangeTheme/ChangeTheme';
+import { Loader } from './components/Loader/index';
+import { Theme } from './components/SwitchTheme/index';
 import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, getTodo } from './store/action/todos';
 // import { AddTodo } from './Todo/AddTodo';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const todos = useSelector((state) => state.todos);
 
-  const getTodos = () => {
-    dispatch({ type: 'GET_TODOS' });
+  const todo = useSelector((state) => state.todos);
+
+  const get = () => {
+    dispatch(getTodo());
   };
 
-  const addTodo = () => {
-    dispatch({ type: 'ADD_TODO' });
+  const add = () => {
+    dispatch(addTodo());
   };
-
-  // const [todos, setTodos] = React.useState([]);
-  // const [loading, setLoading] = React.useState(true);
-
-  // React.useEffect(async () => {
-  //   await fetch('http://localhost:3001/todos')
-  //     .then((response) => response.json())
-  //     .then((todos) => {
-  //       setTodos(todos);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  // const removeTodo = React.useCallback(
-  //   (id) => {
-  //     setTodos(todos.filter((todo) => todo.id !== id));
-  //   },
-  //   [todos]
-  // );
-
-  // const addTodo = React.useCallback(
-  //   (title) => {
-  //     setTodos(
-  //       todos.concat([
-  //         {
-  //           title,
-  //           id: Date.now(),
-  //           completed: false,
-  //         },
-  //       ])
-  //     );
-  //   },
-  //   [todos]
-  // );
 
   return (
-    // <Context.Provider value={{ removeTodo }}>
-    <div className={'app'}>
+    <div className="app">
       <h1>Todos</h1>
 
       <button onClick={() => getTodos()}>Get Todos</button>
@@ -63,18 +30,17 @@ export const App = () => {
 
       <Theme></Theme>
 
-      {/* <React.Suspense fallback={<h2>Loading...</h2>}>
-          <AddTodo onCreate={addTodo}></AddTodo>
-        </React.Suspense> */}
+      <React fallback={<h2>Loading...</h2>}>
+        <AddTodo onCreate={addTodo}></AddTodo>
+      </React>
 
       {loading && <Loader></Loader>}
 
-      {/* {todos.length ? (
-          <TodoList todos={todos}></TodoList>
-        ) : loading ? null : (
-          <h1>No Todos!</h1>
-        )} */}
+      {todos.length ? (
+        <TodoList todos={todos}></TodoList>
+      ) : loading ? null : (
+        <h1>No Todos!</h1>
+      )}
     </div>
-    // </Context.Provider>
   );
 };
