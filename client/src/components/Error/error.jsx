14c06ff2c80e04696c48b64a8d3a99078f0f4649
StarkovAleksandr <starkovlife@gmail.com>
2021-09-13 10:57:46 +0300

    the layout of the error output has been completed

--- client/src/components/Error/error.jsx
+++ client/src/components/Error/error.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import errorImage from '../../../assets/img/error_logo.png';
 
 export const ErrorMessage = () => (
-  <div className={'roller'}>
+  <div className={'error'}>
     <img src={errorImage} alt="Error" />
     <p>Oops, something happend</p>
   </div>
